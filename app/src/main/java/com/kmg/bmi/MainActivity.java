3d081e1c375dfa09e4a7481af05b3ae0cb5b7415
kmg <kmg31815@gmail.com>
2018-10-16 16:29:04 +0800

    help

--- app/src/main/java/com/kmg/bmi/MainActivity.java
+++ app/src/main/java/com/kmg/bmi/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,15 +14,30 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText edWeight;
     private EditText edHeight;
+    private Button help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edWeight = findViewById(R.id.ed_weight);
-        edHeight = findViewById(R.id.ed_height);
+        findviews();
     }
 
+    private void findviews() {
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
+        help = findViewById(R.id.help);
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("onClick","OnClickListener");
+                new AlertDialog.Builder(MainActivity.this)
+                        .setMessage("The body mass index (BMI) or Quetelet index is a value derived from the mass (weight) and height of an individual.")
+                        .setPositiveButton(R.string.ok,null)
+                        .show();
+            }
+        });
+    }
     public void bmi(View view) {
         Log.d("MainActivity", "bmi");
         String w = edWeight.getText().toString();
@@ -31,17 +47,19 @@ public class MainActivity extends AppCompatActivity {
         float height = Float.parseFloat(h);
         float bmi = weight / (height * height);
         Log.d("MainActivity", bmi + "");
-        Toast.makeText(this,"Your BMI is"+bmi,Toast.LENGTH_LONG).show();
+        Toast.makeText(this,getString(R.string.Your_BMI_is)+bmi,Toast.LENGTH_LONG).show();
         new AlertDialog.Builder(this)
                 .setMessage("your BMI is "+bmi)
                 .setTitle("BMI")
-                .setPositiveButton("OK",null)
+                .setPositiveButton("OK", null)
                 .setNegativeButton("Clear", new DialogInterface.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialog, int i) {
+                    public void onClick(DialogInterface dialog, int which) {
                         edWeight.setText("");
                         edHeight.setText("");
                     }
                 }).show();
+
+        }
     }
 }
