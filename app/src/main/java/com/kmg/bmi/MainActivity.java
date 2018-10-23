b6dc5624bf05ed61fb36ee5b32e78dd0ffe21acd
kmg <kmg31815@gmail.com>
2018-10-23 16:26:18 +0800

    LifeActivity

--- app/src/main/java/com/kmg/bmi/MainActivity.java
+++ app/src/main/java/com/kmg/bmi/MainActivity.java
@@ -2,10 +2,12 @@ package com.kmg.bmi;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.ExtractedTextRequest;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -23,6 +25,42 @@ public class MainActivity extends AppCompatActivity {
         findviews();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("MainActivity", "onRestart: ");
+    }
+
     private void findviews() {
         edWeight = findViewById(R.id.ed_weight);
         edHeight = findViewById(R.id.ed_height);
@@ -33,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("onClick","OnClickListener");
                 new AlertDialog.Builder(MainActivity.this)
                         .setMessage("The body mass index (BMI) or Quetelet index is a value derived from the mass (weight) and height of an individual.")
-                        .setPositiveButton(R.string.ok,null)
+                        .setPositiveButton("ok",null)
                         .show();
             }
         });
@@ -47,7 +85,10 @@ public class MainActivity extends AppCompatActivity {
         float height = Float.parseFloat(h);
         float bmi = weight / (height * height);
         Log.d("MainActivity", bmi + "");
-        Toast.makeText(this,getString(R.string.Your_BMI_is)+bmi,Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("BMI" , bmi);
+        startActivity(intent);
+  /*      Toast.makeText(this,"Your BMI is "+bmi,Toast.LENGTH_LONG).show();
         new AlertDialog.Builder(this)
                 .setMessage("your BMI is "+bmi)
                 .setTitle("BMI")
@@ -59,7 +100,6 @@ public class MainActivity extends AppCompatActivity {
                         edHeight.setText("");
                     }
                 }).show();
-
+*/
         }
-    }
 }
